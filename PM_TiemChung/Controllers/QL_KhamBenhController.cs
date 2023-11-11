635358d5c93a3ad811ba52e2a17a92356296a3e7
Nguyễn Thanh Ý <85022353+thanhy123zzz@users.noreply.github.com>
2023-11-11 23:45:44 +0700

    .

--- PM_TiemChung/Controllers/QL_KhamBenhController.cs
+++ PM_TiemChung/Controllers/QL_KhamBenhController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PM_TiemChung.Models.Entities;
 
 namespace PM_TiemChung.Controllers
 {
     [Route("QuanLy/[controller]")]
     public class QL_KhamBenhController : Controller
     {
-        public IActionResult KhamBenh()
+        private ThaiLaiContext _context;
+        private readonly IMapper _mapper;
+        public QL_KhamBenhController(IMapper mapper, ThaiLaiContext context)
         {
+            _context = context;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> KhamBenh()
+        {
+            ViewBag.BenhNhans = await _context.DmBenhNhans
+                .Include(x => x.IdgtNavigation)
+                .Include(x => x.IdtinhNavigation)
+                .Include(x => x.IdquanNavigation)
+                .Include(x => x.IdpxNavigation)
+                .Include(x => x.IddtNavigation)
+                .Include(x => x.IdnnNavigation)
+                .Include(x => x.IdqgNavigation)
+                .Where(x => x.Active == true && x.NgayKham.Value.Date == DateTime.Now.Date).ToListAsync();
             return View();
         }
     }
