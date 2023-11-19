6d7b3394c0a85735f00a0012fac5223007291457
thailai2851 <thailai2851@gmail.com>
2023-11-19 18:00:15 +0700

    .

---
+++ PM_TiemChung/Controllers/HH_NhapKhoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PM_TiemChung.Controllers
+{
+    [Route("HangHoa/[controller]")]
+    public class HH_NhapKhoController : Controller
+    {
+        public IActionResult NhapKho()
+        {
+            return View();
+        }
+    }
+}
