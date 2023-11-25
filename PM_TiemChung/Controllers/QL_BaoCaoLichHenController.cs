36837da71bffa27a158c0fcc274732242df2895e
thailai2851 <thailai2851@gmail.com>
2023-11-25 15:57:13 +0700

    .

---
+++ PM_TiemChung/Controllers/QL_BaoCaoLichHenController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PM_TiemChung.Controllers
+{
+    [Route("QuanLy/[controller]")]
+
+    public class QL_BaoCaoLichHenController : Controller
+    {
+        public IActionResult BaoCaoLichHen()
+        {
+            return View();
+        }
+    }
+}
